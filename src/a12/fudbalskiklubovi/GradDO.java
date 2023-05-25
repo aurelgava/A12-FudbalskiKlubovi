38530bc8494f9ce0072f2c5533267641001d116d
Kab15prof <Kab15prof@Prof>
2023-05-25 16:53:01 +0200

    Prva strana bez search dugmeta

---
+++ src/a12/fudbalskiklubovi/GradDO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a12.fudbalskiklubovi;
+
+/**
+ *
+ * @author Kab15prof
+ */
+class GradDO {
+    int ID;
+    String naziv;
+
+    @Override
+    public String toString() {
+        return naziv;
+    }
+    
+    
+}
